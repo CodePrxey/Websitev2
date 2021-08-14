1c6c02ee3bc67418146a94e5e80599d59ea49313
CodePrxey <88936987+CodePrxey@users.noreply.github.com>
2021-08-14 18:04:04 +0300

    Update pievieno_tehniku.js

--- js/pievieno_tehniku.js
+++ js/pievieno_tehniku.js
@@ -1,1 +1,34 @@
-alert(1)
+//javascript funkcija, kas pievieno datus json failam, ja html lapā nospiež pogu sablabāt
+
+async function pievieno_tehniku()
+{
+let requestBodyJson; 
+//izveido json datu struktūru no html lapas input elementu vērtībām
+    requestBodyJson = { 
+      // "skaits": parseInt(document.querySelector('#skaits').value),            
+      "augatips": document.querySelector('#inos1').value, 
+      "daudzums": document.querySelector('#inos2').value,
+      "maksa": document.querySelector('#inos3').value,                
+      }                          
+            
+let requestBodyString = JSON.stringify(requestBodyJson);
+
+let request = await fetch('https://replit.com/@CodePrxey/Websitev2/api/pievienot',
+         		{
+            method:"POST",
+  		headers:
+  		{            
+            'Content-Type': 'application/json'
+            },
+            body:requestBodyString
+        		}).then(response => response.json())
+            .then(data => {
+                if(data.status == 0) {
+                    alert("Kļūda")
+                }
+                if(data.status == 1) {
+                    alert("ieraksts pievienots")
+                }
+            })
+location.reload();
+}

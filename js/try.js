65deb8be39d55fc19517372c1b074c591de48e96
CodePrxey <88936987+CodePrxey@users.noreply.github.com>
2021-10-29 17:22:26 +0300

    Update try.js

--- js/try.js
+++ js/try.js
@@ -1,1 +1,19 @@
-
+let myObj
+let text
+let x
+async function change_myselect(sel) {
+const dbParam = roomJson.stringify({table:sel,limit:20});
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  myObj = roomJson.parse(this.responseText);
+  text = "<table border='1'>"
+  for (x in myObj) {
+    text += "<tr><td>" + myObj[x].name + "</td></tr>";
+  }
+  text += "</table>"    
+  document.getElementById("demo").innerHTML = text;
+}
+xmlhttp.open("POST", "json_demo_html_table.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
+}

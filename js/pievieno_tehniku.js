de8f19b0faa821c3927e55f8444bc297f95399aa
CodePrxey <88936987+CodePrxey@users.noreply.github.com>
2021-10-29 17:10:32 +0300

    Update pievieno_tehniku.js

--- js/pievieno_tehniku.js
+++ js/pievieno_tehniku.js
@@ -42,6 +42,23 @@ if (document.querySelector('#izv').value == 1) {
 if (document.querySelector('#izv').value == 2){
     document.querySelector('#ask').value = darzSkaits * 2;   
 }
+
+const dbParam = roomJSON.stringify({table:sel,limit:20});
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  myObj = JSON.parse(this.responseText);
+  text = "<table border='1'>"
+  for (x in myObj) {
+    text += "<tr><td>" + myObj[x].name + "</td></tr>";
+  }
+  text += "</table>"    
+  document.getElementById("demo").innerHTML = text;
+}
+xmlhttp.open("POST", "json_demo_html_table.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
+
+
 //alert(kabinetuSkaits)
 
 /*let request = await fetch('https://con.codeprxey.repl.co/api/augi',
